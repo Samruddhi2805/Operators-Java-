c431807986a91b44a743e9eb6ed0b7012b7cd1fd
Samruddhi Nevse <nevsesamruddhi@gmail.com>
2025-08-21 20:23:22 +0530

    Add files via upload

---
+++ operate.java
@@ -0,0 +1,8 @@
+import java.util.* ;
+
+public static (String args[]){
+Scanner scan=new scanner(System.in);
+String s1=new String("Hello");
+String s2=new String("Hello");
+System.out.println(s1==s2);
+System.out.println(s1.equals(s2));}
